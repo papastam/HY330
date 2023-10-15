32adbf0e9d2c362eb2dc4f539e5c5187ecdd1bd4
Chris Papastamos <xristos.papastamos@gmail.com>
2023-10-15 16:28:41 +0300

    almost done

--- assignment1/lab0_2.py
+++ assignment1/lab0_2.py
@@ -6,7 +6,7 @@
 #
 # GNU Radio Python Flow Graph
 # Title: Not titled yet
-# Author: zoe
+# Author: chris
 # GNU Radio version: 3.10.5.1
 
 from packaging.version import Version as StrictVersion
@@ -34,14 +34,12 @@ import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-from gnuradio.qtgui import Range, RangeWidget
-from PyQt5 import QtCore
 
 
 
 from gnuradio import qtgui
 
-class lab0_1(gr.top_block, Qt.QWidget):
+class lab0_2(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
@@ -64,7 +62,7 @@ class lab0_1(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "lab0_1")
+        self.settings = Qt.QSettings("GNU Radio", "lab0_2")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -78,20 +76,16 @@ class lab0_1(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 32000
-        self.frequency = frequency = 1000
 
         ##################################################
         # Blocks
         ##################################################
 
-        self._frequency_range = Range(1000, 5000, 10, 1000, 200)
-        self._frequency_win = RangeWidget(self._frequency_range, self.set_frequency, "Frequency", "counter_slider", int, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._frequency_win)
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
             1024, #size
             samp_rate, #samp_rate
             "", #name
-            1, #number of inputs
+            4, #number of inputs
             None # parent
         )
         self.qtgui_time_sink_x_0.set_update_time(0.10)
@@ -108,11 +102,11 @@ class lab0_1(gr.top_block, Qt.QWidget):
         self.qtgui_time_sink_x_0.enable_stem_plot(False)
 
 
-        labels = ['Ex_1_Signal', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+        labels = ['Sine', 'Triangle', 'Sawtooth', 'Square', 'Signal 5',
             'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
         widths = [1, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
-        colors = ['magenta', 'red', 'green', 'black', 'cyan',
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
             'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
             1.0, 1.0, 1.0, 1.0, 1.0]
@@ -122,12 +116,9 @@ class lab0_1(gr.top_block, Qt.QWidget):
             -1, -1, -1, -1, -1]
 
 
-        for i in range(2):
+        for i in range(4):
             if len(labels[i]) == 0:
-                if (i % 2 == 0):
-                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
-                else:
-                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
                 self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
             self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
@@ -138,19 +129,31 @@ class lab0_1(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.analog_sig_source_x_0_0_1 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, frequency, 2, 0, 3.14159265359)
+        self.blocks_throttle_0_1_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_throttle_0_1 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_throttle_0_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.analog_sig_source_x_1_0_0 = analog.sig_source_f(samp_rate, analog.GR_SQR_WAVE, 4000, 4, 0, 0)
+        self.analog_sig_source_x_1_0 = analog.sig_source_f(samp_rate, analog.GR_SAW_WAVE, 3000, 3, 0, 0)
+        self.analog_sig_source_x_1 = analog.sig_source_f(samp_rate, analog.GR_TRI_WAVE, 2000, 2, 0, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, 1000, 1, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0_0_1, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.analog_sig_source_x_1, 0), (self.blocks_throttle_0_0, 0))
+        self.connect((self.analog_sig_source_x_1_0, 0), (self.blocks_throttle_0_1, 0))
+        self.connect((self.analog_sig_source_x_1_0_0, 0), (self.blocks_throttle_0_1_0, 0))
         self.connect((self.blocks_throttle_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_throttle_0_0, 0), (self.qtgui_time_sink_x_0, 1))
+        self.connect((self.blocks_throttle_0_1, 0), (self.qtgui_time_sink_x_0, 2))
+        self.connect((self.blocks_throttle_0_1_0, 0), (self.qtgui_time_sink_x_0, 3))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "lab0_1")
+        self.settings = Qt.QSettings("GNU Radio", "lab0_2")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -162,21 +165,20 @@ class lab0_1(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0_0_1.set_sampling_freq(self.samp_rate)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_1_0.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_1_0_0.set_sampling_freq(self.samp_rate)
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
-
-    def get_frequency(self):
-        return self.frequency
-
-    def set_frequency(self, frequency):
-        self.frequency = frequency
-        self.analog_sig_source_x_0_0_1.set_frequency(self.frequency)
+        self.blocks_throttle_0_0.set_sample_rate(self.samp_rate)
+        self.blocks_throttle_0_1.set_sample_rate(self.samp_rate)
+        self.blocks_throttle_0_1_0.set_sample_rate(self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
 
 
 
-def main(top_block_cls=lab0_1, options=None):
+def main(top_block_cls=lab0_2, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
