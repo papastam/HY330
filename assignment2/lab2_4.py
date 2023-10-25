1bbfc71ee5d6602302ae6cd414fbe0b6b18d2c37
Chris Papastamos <xristos.papastamos@gmail.com>
2023-10-25 14:05:12 +0300

    progress

--- assignment2/lab2_4.py
+++ assignment2/lab2_4.py
@@ -27,6 +27,7 @@ from gnuradio.filter import firdes
 import sip
 from gnuradio import analog
 from gnuradio import blocks
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio.fft import window
 import sys
@@ -41,7 +42,7 @@ from PyQt5 import QtCore
 
 from gnuradio import qtgui
 
-class lab2_1(gr.top_block, Qt.QWidget):
+class lab2_4(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
@@ -64,7 +65,7 @@ class lab2_1(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "lab2_1")
+        self.settings = Qt.QSettings("GNU Radio", "lab2_4")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -77,35 +78,28 @@ class lab2_1(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 98000
-        self.S2_FREQ = S2_FREQ = 6000
-        self.S2_AMP = S2_AMP = 2
-        self.S1_FREQ = S1_FREQ = 6000
-        self.S1_AMP = S1_AMP = 2
+        self.samp_rate = samp_rate = 32000
+        self.frequency = frequency = 1000
 
         ##################################################
         # Blocks
         ##################################################
 
-        self._S2_FREQ_range = Range(1000, 48000, 1000, 6000, 200)
-        self._S2_FREQ_win = RangeWidget(self._S2_FREQ_range, self.set_S2_FREQ, "'S2_FREQ'", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._S2_FREQ_win)
-        self._S2_AMP_range = Range(0, 10, 1, 2, 200)
-        self._S2_AMP_win = RangeWidget(self._S2_AMP_range, self.set_S2_AMP, "'S2_AMP'", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._S2_AMP_win)
-        self._S1_FREQ_range = Range(1000, 48000, 1000, 6000, 200)
-        self._S1_FREQ_win = RangeWidget(self._S1_FREQ_range, self.set_S1_FREQ, "'S1_FREQ'", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._S1_FREQ_win)
-        self._S1_AMP_range = Range(0, 10, 1, 2, 200)
-        self._S1_AMP_win = RangeWidget(self._S1_AMP_range, self.set_S1_AMP, "'S1_AMP'", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._S1_AMP_win)
+        self._frequency_range = Range(1000, 32000, 100, 1000, 200)
+        self._frequency_win = RangeWidget(self._frequency_range, self.set_frequency, "'frequency'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._frequency_win)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=4,
+                decimation=1,
+                taps=[],
+                fractional_bw=0)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_f(
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
             0, #fc
             samp_rate, #bw
             "", #name
-            3,
+            2,
             None # parent
         )
         self.qtgui_freq_sink_x_0.set_update_time(0.10)
@@ -120,9 +114,9 @@ class lab2_1(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_0.set_fft_window_normalized(False)
 
 
-        self.qtgui_freq_sink_x_0.set_plot_pos_half(not False)
+        self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
 
-        labels = ['S1', 'S2', 'S1+S2', '', '',
+        labels = ['Original Signal (32k sampling rate)', 'Down sampled Signal (32k/4=8k sampling rate)', '', '', '',
             '', '', '', '', '']
         widths = [1, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
@@ -131,7 +125,7 @@ class lab2_1(gr.top_block, Qt.QWidget):
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
             1.0, 1.0, 1.0, 1.0, 1.0]
 
-        for i in range(3):
+        for i in range(2):
             if len(labels[i]) == 0:
                 self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
@@ -143,24 +137,26 @@ class lab2_1(gr.top_block, Qt.QWidget):
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
-        self.blocks_add_xx_0 = blocks.add_vff(1)
-        self.analog_sig_source_x_0_0_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, S1_FREQ, S1_AMP, 0, 0)
-        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, S2_FREQ, S2_AMP, 0, 0)
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_deinterleave_0 = blocks.deinterleave(gr.sizeof_float*1, 1)
+        self.analog_sig_source_x_0_0_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, frequency, 2, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_add_xx_0, 1))
-        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.analog_sig_source_x_0_0_0, 0), (self.blocks_add_xx_0, 0))
+        self.connect((self.analog_sig_source_x_0_0_0, 0), (self.blocks_deinterleave_0, 0))
         self.connect((self.analog_sig_source_x_0_0_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.blocks_add_xx_0, 0), (self.qtgui_freq_sink_x_0, 2))
-        self.connect((self.blocks_throttle_0, 0), (self.qtgui_freq_sink_x_0, 1))
+        self.connect((self.blocks_deinterleave_0, 1), (self.blocks_null_sink_0, 0))
+        self.connect((self.blocks_deinterleave_0, 3), (self.blocks_null_sink_0, 2))
+        self.connect((self.blocks_deinterleave_0, 2), (self.blocks_null_sink_0, 1))
+        self.connect((self.blocks_deinterleave_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.qtgui_freq_sink_x_0, 1))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "lab2_1")
+        self.settings = Qt.QSettings("GNU Radio", "lab2_4")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -172,43 +168,21 @@ class lab2_1(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
         self.analog_sig_source_x_0_0_0.set_sampling_freq(self.samp_rate)
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
 
-    def get_S2_FREQ(self):
-        return self.S2_FREQ
+    def get_frequency(self):
+        return self.frequency
 
-    def set_S2_FREQ(self, S2_FREQ):
-        self.S2_FREQ = S2_FREQ
-        self.analog_sig_source_x_0_0.set_frequency(self.S2_FREQ)
-
-    def get_S2_AMP(self):
-        return self.S2_AMP
-
-    def set_S2_AMP(self, S2_AMP):
-        self.S2_AMP = S2_AMP
-        self.analog_sig_source_x_0_0.set_amplitude(self.S2_AMP)
-
-    def get_S1_FREQ(self):
-        return self.S1_FREQ
-
-    def set_S1_FREQ(self, S1_FREQ):
-        self.S1_FREQ = S1_FREQ
-        self.analog_sig_source_x_0_0_0.set_frequency(self.S1_FREQ)
-
-    def get_S1_AMP(self):
-        return self.S1_AMP
-
-    def set_S1_AMP(self, S1_AMP):
-        self.S1_AMP = S1_AMP
-        self.analog_sig_source_x_0_0_0.set_amplitude(self.S1_AMP)
+    def set_frequency(self, frequency):
+        self.frequency = frequency
+        self.analog_sig_source_x_0_0_0.set_frequency(self.frequency)
 
 
 
 
-def main(top_block_cls=lab2_1, options=None):
+def main(top_block_cls=lab2_4, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
