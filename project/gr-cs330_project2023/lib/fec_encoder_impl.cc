7aeee4a2cd232958396a1f4d153e53ecbb3a3c40
Chris Papastamos <xristos.papastamos@gmail.com>
2023-12-18 10:03:39 +0200

    messed up the files

--- project/gr-cs330_project2023/lib/fec_encoder_impl.cc
+++ project/gr-cs330_project2023/lib/fec_encoder_impl.cc
@@ -53,20 +53,44 @@ namespace gr {
 void
 fec_encoder_impl::encode(pmt::pmt_t m)
 {
+    uint8_t *bytes_out = nullptr;
+    uint8_t *bits_out = nullptr;
+    pmt::pmt_t bytes(pmt::cdr(m));
+
     switch (d_type) {
-    /* No FEC just copy the input message to the output */
-    case 0:
-        message_port_pub(pmt::mp("pdu_out"), m);
-        return;
-    case 1:
-        /* Do Hamming encoding */
-        return;
-    case 2:
-        /* Do Golay encoding */
-        return;
-    default:
-        throw std::runtime_error("fec_encoder: Invalid FEC");
-        return;
+        /* No FEC just copy the input message to the output */
+        case 0:
+            message_port_pub(pmt::mp("pdu_out"), m);
+            return;
+        case 1:
+            size_t pdu_length;
+            const uint8_t *bytes_in = pmt::u8vector_elements(bytes, pdu_length);
+            
+            bytes_out = new uint8_t[pdu_length*3];
+            bits_out = new uint8_t[(pdu_length*3)*8];
+
+            for(int byteindex = 0; byteindex < pdu_length; byteindex++){
+                for(int bitindex = 0; bitindex < 8; bitindex++){
+                    bits_out[byteindex*3]   = (bytes_in[byteindex] & (0x01 << bitindex)) >> bitindex;
+                    bits_out[byteindex*3+1] = (bytes_in[byteindex] & (0x01 << bitindex)) >> bitindex;
+                    bits_out[byteindex*3+2] = (bytes_in[byteindex] & (0x01 << bitindex)) >> bitindex;
+                }
+            }
+
+            for(int byteindex = 0; byteindex < pdu_length*3; byteindex++){
+                for(int bitindex = 0; bitindex < 8; bitindex++){
+                    bytes_out[byteindex] |= bits_out[byteindex*8+bitindex] << bitindex;
+                }
+            }
+
+            message_port_pub(pmt::mp("pdu_out"), pmt::init_u8vector(pdu_length*3, bytes_out));
+            return;
+        case 2:
+            /* Do Golay encoding */
+            return;
+        default:
+            throw std::runtime_error("fec_encoder: Invalid FEC");
+            return;
     }
 }
 
